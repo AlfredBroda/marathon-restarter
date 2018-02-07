a6d87b2c5c4030665d4a9bb604a0c99b796c3f89
Alfred Broda <alfred.broda@allegro.pl>
2018-02-07 13:12:38 +0100

    more tests

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/gambol99/go-marathon"
 	"time"
+	"fmt"
 )
 
 type MockClient struct {
@@ -15,6 +16,9 @@ type MockClient struct {
 
 func (m *MockClient) RestartApplication(appID string, force bool) (*marathon.DeploymentID, error) {
 	args := m.Called(appID, force)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
 	result := args.Get(0).(marathon.DeploymentID)
 	return &result, args.Error(1)
 }
@@ -24,29 +28,47 @@ func (m *MockClient) WaitOnDeployment(deployID string, timeout time.Duration) (e
 	return args.Error(0)
 }
 
-func TestIfRestartsAllGivenApplications(t *testing.T) {
-	mockClient := new(MockClient)
-
-	firstAppID := "/test/first.app"
-	secondAppID := "/test/second.app"
-	timeout := 120 * time.Second
-	apps := []marathon.Application{
+var (
+	firstAppID  = "/test/first.app"
+	secondAppID = "/test/second.app"
+	testApps    = []marathon.Application{
 		{ID: firstAppID},
 		{ID: secondAppID},
 	}
-	deployOne := marathon.DeploymentID{DeploymentID: firstAppID}
-	deployTwo := marathon.DeploymentID{DeploymentID: secondAppID}
+	deployOne = marathon.DeploymentID{DeploymentID: firstAppID}
+	deployTwo = marathon.DeploymentID{DeploymentID: secondAppID}
+)
+
+func TestIfRestartsAllGivenApplications(t *testing.T) {
+	mockClient := new(MockClient)
 
 	mockClient.On("RestartApplication", firstAppID, false).Return(deployOne, nil).Once()
 	mockClient.On("RestartApplication", secondAppID, false).Return(deployTwo, nil).Once()
 
 	mockClient.On("WaitOnDeployment", firstAppID, timeout).Return(nil).Once()
-	mockClient.On("WaitOnDeployment", secondAppID,timeout).Return(nil).Once()
+	mockClient.On("WaitOnDeployment", secondAppID, timeout).Return(nil).Once()
 
 	// when
-	remaining := restartApps(apps, mockClient)
+	remaining := restartApps(testApps, mockClient)
 
 	// then
 	require.Empty(t, remaining)
+	mockClient.AssertExpectations(t)
+}
+
+func TestIfRestartErrorsAreRetryable(t *testing.T) {
+	mockClient := new(MockClient)
+
+	mockClient.On("RestartApplication", firstAppID, false).Return(nil, fmt.Errorf("test error")).Once()
+	mockClient.On("RestartApplication", secondAppID, false).Return(deployTwo, nil).Once()
+
+	mockClient.On("WaitOnDeployment", secondAppID, timeout).Return(nil).Once()
+
+	// when
+	remaining := restartApps(testApps, mockClient)
+
+	// then
+	require.NotEmpty(t, remaining)
+	require.Contains(t, remaining, testApps[0])
 	mockClient.AssertExpectations(t)
 }
